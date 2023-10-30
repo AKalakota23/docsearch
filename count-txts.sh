834829b8b1e3d11f27e9e8bc00ba59c917890676
AKalakota23 <akhilkalakota@gmail.com>
2023-10-30 15:36:01 -0700

    added stuff

---
+++ count-txts.sh
@@ -0,0 +1,11 @@
+find technical > find-results.txt
+grep ".txt" find-results.txt > grep-results.txt
+wc grep-results.txt
+
+find technical/biomed > find-biomed.txt
+grep ".txt" find-biomed.txt > grep-biomed.txt
+wc grep-biomed.txt
+
+find technical/plos > find-plos.txt
+grep ".txt" find-plos.txt > grep-plos.txt
+wc grep-plos.txt
